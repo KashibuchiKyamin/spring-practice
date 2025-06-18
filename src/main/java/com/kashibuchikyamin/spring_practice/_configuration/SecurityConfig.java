7114e69b8526b63241e7b5954f9df554ef92e264
kyamin <y.mrkm.0722@gmail.com>
2025-06-18 23:54:41 +0900

    ちょい修正

--- src/main/java/com/kashibuchikyamin/spring_practice/_configuration/SecurityConfig.java
+++ src/main/java/com/kashibuchikyamin/spring_practice/_configuration/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.kashibuchikyamin.spring_practice._configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * セキュリティ設定を行うクラスです。
@@ -13,12 +15,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig {
 
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers("/login", "/css/**", "/js/**", "/static/**").permitAll()
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.loginProcessingUrl("/login") // POST先
+						.defaultSuccessUrl("/top", true)
+						.permitAll())
+				.logout(logout -> logout.permitAll());
+
+		return http.build();
+	}
+
 	/** 
 	 * パスワードエンコーダーを定義します。
 	 * @return PasswordEncoderのインスタンス
 	 */
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+		System.out.println(bCryptPasswordEncoder.encode("p@aaword"));
+
+		return bCryptPasswordEncoder;
 	}
 }
